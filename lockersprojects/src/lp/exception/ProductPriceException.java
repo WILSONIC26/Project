1f9c0d1e37cdbc7207dab90e34955ec64ddda848
WILSONIC26 <john.j.wilson86@gmail.com>
2023-08-30 09:05:12 +0100

    Add to git hub

---
+++ lockersprojects/src/lp/exception/ProductPriceException.java
@@ -0,0 +1,11 @@
+package lp.exception;
+
+public class ProductPriceException extends Exception{
+	public ProductPriceException() {
+		// TODO Auto-generated constructor stub
+	}
+	public ProductPriceException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
